f3ca9efea0c7aefdbcc7e4c6722d2ae5bb1d2b73
Flamislas <melvin.hersent@gmail.com>
2020-05-03 15:26:52 +0200

    overlapse verification added//Seed does not work anymore

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,19 +2,27 @@ class Reservation < ApplicationRecord
   belongs_to :guest, class_name: "User"
   belongs_to :listing
 
-  validates overlaping_reservation?
+  validates :start_date,
+  presence: true
+
+  validates :end_date,
+  presence: true
+
+  validate :overlaping_reservation?
 
   def duration
    end_date - start_date
   end
 
-  # wrong on so many levels.
-=begin
-  def overlaping_reservation?(datetime)
-    if (Reservation.where("(? BETWEEN date_start AND date_end OR ? BETWEEN date_start AND date_end) AND user_id = ?", self.date_start, self.date_end, self.user_id).any?)
-      errors.add(:date_end, 'it overlaps another reservation')
-   end
+  def period
+    start_date..end_date
   end
-=end
 
+  def overlaping_reservation?
+    other_reservations = Reservation.all
+    is_overlapping = other_reservations.any? do |other_booking|
+      period.overlaps?(other_booking.period)
+    end
+    errors.add(:overlaps_with_other) if is_overlapping
+  end
 end
